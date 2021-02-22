6396009a4c16880f3ae826c83e5a330488758d3e
kuldeep <kuldeep.yadav@noosyntech.in>
2021-02-22 22:25:51 +0530

    Removed horrizontal divider from post list

--- src/components/posts.js
+++ src/components/posts.js
@@ -6,17 +6,19 @@ const Tag = ({name}) => (
 )
 
 const Post = ({ title, date, timeToRead, excerpt, tags, slug }) => (
-    <div className="flex flex-col border-b-2 border-gray-100 p-4">
-            <Link className="inline-block text-gray-500 my-2 text-xl font-semibold" to={`${slug}`}>{title}</Link>
-            <div className="flex text-gray-400 mb-2">
-              <small className="text-gray-400 mr-4">{date}</small>
-              <small className="text-gray-400">{`${timeToRead} min read`}</small>
+    <>
+      <div className="flex flex-col p-4">
+            <Link className="inline-block text-gray-600 my-2 text-xl font-semibold" to={`${slug}`}>{title}</Link>
+            <div className="flex text-gray-500 mb-2">
+              <small className="text-gray-500 mr-4">{date}</small>
+              <small className="text-gray-500">{`${timeToRead} min read`}</small>
             </div>
             <p className="text-gray-600 mb-2">{excerpt}</p>
             <div className="flex text-green-500">
                {tags.map(tag => <Tag key={tag} name={tag} />)}
             </div>
           </div>
+    </>
 )
 
 const Posts = ({ posts }) => posts.map(post => <Post key={post.id} {...post} />)
