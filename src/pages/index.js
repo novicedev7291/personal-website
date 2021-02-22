cf5a5b4656abc525a33b7187f5f55f895d96ab62
kuldeep <kuldeep.yadav@noosyntech.in>
2021-02-22 09:41:26 +0530

    Fix slug issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = ({ data, location }) => {
       date: node.frontmatter.date,
       tags: node.frontmatter.tags.split(','),
       timeToRead: node.timeToRead,
-      slug: node.slug
+      slug: node.fields.slug
     }
   })
 
