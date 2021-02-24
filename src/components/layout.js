e3644d000d01f94b86aaf9deec20a6be3282f64e
kuldeep <kuldeep.yadav@noosyntech.in>
2021-02-24 23:49:55 +0530

    Fix few layout issues

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 const DefaultLayout = ({ children }) => (
-  <div className="max-w-3xl mx-auto bg-gray-50 h-screen px-8">{children}</div>
+  <>
+    <div className="max-w-3xl mx-auto bg-gray-50 h-screen px-8">
+      {children}
+    </div>
+  </>
 )
 
 export default DefaultLayout
